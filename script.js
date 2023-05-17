d6482bd11918ea3d43f72f2cc87e22dac4505bc4
minhaj539 <115364661+minhaj539@users.noreply.github.com>
2023-05-17 18:59:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // complete this js code
 function Person(name, age) {}
+Person.prototype.greet=function(){
+	console.log("Hello, my name is "+this.name+" I am "+this.age+" years old.")
+}
 
 function Employee(name, age, jobTitle) {}
+Employee.prototype.greet=function(){
+	console.log("Hello, my name is "+this.name+" I am "+this.age+" years old, and my job title is "+this.jobTitle);
+}
 
 // Do not change code below this line
 window.Person = Person;
